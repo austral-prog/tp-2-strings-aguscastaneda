a6f2bcf8b2c86ba21660e2c68e988866f1e4ca2a
aguscastaneda <castaneda.agustin.et21.21@gmail.com>
2026-03-15 14:10:07 -0300

    intento 1
    Updated input prompts and modified print format.

--- names.py
+++ names.py
@@ -2,12 +2,12 @@ def names():
     """Lee nombre y apellido, e imprime el nombre completo en distintos formatos:
     minúsculas, título, mayúsculas y con tabulador.
     """
-    nombre = input("Ingrese su nombre: ")
-    apellido = input("Ingrese su apellido: ")
+    nombre = input()
+    apellido = input()
+
     nombre_completo = f"{nombre} {apellido}"
+
     print(nombre_completo.lower())
     print(nombre_completo.title())
     print(nombre_completo.upper())
-    print(f"\t{nombre_completo}")
-
-
+    print(f"\t{nombre_completo.lower()}")
